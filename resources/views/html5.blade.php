d5e47d485b2662f0b2e7db949b3f8b30da769c72
erba daniele <erba86.de@gmail.com>
2023-05-25 20:19:14 +0200

    html5

--- resources/views/html5.blade.php
+++ resources/views/html5.blade.php
@@ -13,14 +13,76 @@
 
     <main>
 
+<section>
+
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="descrHtml">
-                <p>HTML5 è un linguaggio di markup per la strutturazione delle pagine web, pubblicato come W3C Recommendation da ottobre 2014. Il suo obiettivo è quello di fornire una soluzione più semplice per la pubblicazione di contenuti web complessi e per migliorare l'interoperabilità di applicazioni e script tra diversi dispositivi (ad esempio, PC, tablet, smartphone, TV, e così via). HTML5 è il quinto HTML standardizzato, ma la prima versione ad essere stato progettato per essere utilizzato non solo in computer e dispositivi desktop, ma anche su smartphone, tablet, e dispositivi mobili. Alcune delle nuove funzionalità sono state create per rendere più facile la creazione di applicazioni web con funzionalità avanzate come l'elaborazione grafica 2D e 3D, l'accesso al disco rigido locale, audio e video, e la possibilità di lavorare anche offline.</p>
+            <div class="descrHtml1">
+                <p><mark><dfn>HTML5</dfn></mark> è un linguaggio di markup per la strutturazione delle pagine web alla versione 5, pubblicato come W3C Recommendation da ottobre 2014.<u>Il termine</u> <abbr title="HyperText Markup Language">HTML</abbr> <u>significa <q>HyperText Markup Language</u></q>. Il suo obiettivo è quello di fornire una soluzione più semplice per la pubblicazione di contenuti web complessi e per migliorare l'interoperabilità di applicazioni e script tra diversi dispositivi (ad esempio, PC, tablet, smartphone, TV, e così via). <b>HTML5</b> è il quinto <strong>HTML</strong> standardizzato, ma la prima versione ad essere stato progettato per essere utilizzato non solo in computer e dispositivi desktop, ma anche su smartphone, tablet, e dispositivi mobili. Alcune delle nuove funzionalità sono state create per rendere più facile la creazione di applicazioni web con funzionalità avanzate come l'elaborazione grafica 2D e 3D, l'accesso al disco rigido locale, audio e video, e la possibilità di lavorare anche offline.</p>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="descrHtml2">
+                <p><ins> IN HTML, I CONTENUTI SONO ORGANIZZATI IN ELEMENTI,CHE SONO IDENTIFICATI DA TAG</ins>.<br/><mark class="markTag"> I TAG forniscono istruzioni al browser su come interpretare e visualizzare i diversi elementi sulla pagina.</mark> Ogni tag ha una struttura che comprende un tag di apertura e uno di chiusura, racchiudendo il contenuto all'interno.</p>
+                <p>Oltre agli elementi di base come titoli, paragrafi, liste e immagini, HTML offre anche la possibilità di creare collegamenti ipertestuali tra le pagine web tramite l'uso dei tag <a>. Inoltre, è possibile incorporare elementi multimediali come video e audio utilizzando gli appositi tag.</p>
+                <p>HTML permette anche di formattare il testo e applicare stili attraverso l'utilizzo di attributi e tag speciali. Tuttavia, per la presentazione visiva più avanzata, è spesso necessario combinare HTML con CSS (Cascading Style Sheets).</p>
+                <p>Un'altra caratteristica importante di HTML è la sua capacità di supportare moduli e interattività tramite l'utilizzo di form e script. È possibile creare moduli per raccogliere dati da parte degli utenti e inviarli a un server per l'elaborazione.</p>
+                <p>Compilando correttamente un documento HTML, gli elementi vengono interpretati dai browser web, che li renderizzano in modo appropriato sulla pagina visualizzata dagli utenti. Questo rende HTML una parte fondamentale nello sviluppo di qualsiasi sito web moderno.</p>
             </div>
         </div>
-
     </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="descrHtml3">
+                <p>HTML5 è stato progettato per risolvere alcuni dei problemi che affliggevano le precedenti versioni di HTML e per rispondere alle esigenze dei siti web moderni. Alcune delle nuove funzionalità di HTML5 includono:</p>
+                <ul>
+                    <li>Supporto per elementi multimediali come video, audio e canvas.</li>
+                    <li>Supporto per elementi grafici e animazioni 2D e 3D.</li>
+                    <li>Supporto per applicazioni web offline.</li>
+                    <li>Supporto per la geolocalizzazione.</li>
+                    <li>Supporto per la memorizzazione locale.</li>
+                    <li>Supporto per la comunicazione in tempo reale.</li>
+                    <li>Supporto per la storia di navigazione.</li>
+                    <li>Supporto per la creazione di applicazioni web.</li>
+                </ul>
+                <p>HTML5 è stato progettato per essere utilizzato su tutti i dispositivi, dai PC ai tablet e smartphone. Ciò significa che i siti web possono essere visualizzati su una vasta gamma di dispositivi, senza dover creare versioni separate per ciascuno di essi.</p>                            
+          
+
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="descrHtml2">
+                <p>Ecco una lista delle categorie di tag HTML comuni che puoi utilizzare per organizzare il tuo contenuto:<p>
+                <ul></ul>
+                    <li><strong>Tag di layout</strong> - Questi tag vengono utilizzati per creare la struttura e il layout della pagina, ad esempio div per i contenitori generici, header per l'intestazione, nav per la navigazione, main per il contenuto principale, footer per il piè di pagina, section per le sezioni di contenuto, article per gli articoli autonomi, etc.</li>
+                    <li><strong>Tag di intestazione</strong> - Questi tag forniscono informazioni sul documento, come il titolo della pagina, i collegamenti alle risorse esterne e i metadati.</li>
+                    <li><strong>Tag di paragrafo</strong> - Questi tag sono utilizzati per creare paragrafi di testo.</li>
+                    <li><strong>Tag di formattazione</strong> - Questi tag sono utilizzati per formattare il testo, ad esempio per rendere il testo in grassetto o corsivo.</li>
+                    <li><strong>Tag di collegamento</strong> - Questi tag sono utilizzati per creare collegamenti ipertestuali tra le pagine web.</li>
+                    <li><strong>Tag di elenco</strong> - Questi tag sono utilizzati per creare elenchi di elementi.</li>
+                    <li><strong>Tag di immagine</strong> - Questi tag sono utilizzati per incorporare immagini nella pagina web.</li>
+                    <li><strong>Tag di tabella</strong> - Questi tag sono utilizzati per creare tabelle di dati.</li>
+                    <li><strong>Tag di form</strong> - Questi tag sono utilizzati per creare moduli per raccogliere dati da parte degli utenti.</li>
+                    <li><strong>Tag di script</strong> - Questi tag sono utilizzati per incorporare script, come JavaScript, nella pagina web.</li>
+                    
+                </ul>
+
+
+            </div>
+        </div>
+    </div>            
+    
+            </div>
+        </div>
+    </div>
+</section>
+
+
 
 </main>
 
